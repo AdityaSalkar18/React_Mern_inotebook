8b1dcddf9f789739acf11c3d507d1747cad277c0
Aditya <aditysalkar1806@gmail.com>
2024-06-22 16:05:47 +0530

    22.06.2024(v59)

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,10 @@
-import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
+import React from 'react'
+
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(() => {
-    a.update()
-    // eslint-disable-next-line
-  }, [])
+  
   return (
     <div>
-      this is About {a.state.name} and he is in class {a.state.class}
+      this is About page
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,23 +1,10 @@
 
 import NoteContext from "./noteContext";
-import { useState } from "react";
-const NoteState = (props)=>{
-    const s1 = {
-        "name": "Aditya",
-        "class": "5b"
-    }
 
-    const [state, setState] = useState(s1);
-    const update = ()=>{
-        setTimeout(() =>{
-            setState({
-                "name": "Aditya Salkar",
-                 "class": "10b"
-            })
-        }, 1000);
-    }
+const NoteState = (props)=>{
+    
     return(
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
