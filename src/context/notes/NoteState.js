82a221d06e76156a4b538341a906f12b4764ee3a
Aditya <aditysalkar1806@gmail.com>
2024-06-25 15:53:35 +0530

    25-06-20224(v63)

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -107,9 +107,35 @@ const NoteState = (props)=>{
           ]
           const [notes, setNotes] = useState(notesInitial)
     
+          //Add a Note
+          const addNote = (title, description, tag) =>{
+            // TODO API Call
+            console.log("Adding a new note")
+            const note = {
+              "_id": "6676a9e701e12c2b340b075e",
+              "user": "66729b21a151302214dbdbe6",
+              "title": title,
+              "description": description,
+              "tag": tag,
+              "date": "2024-06-22T10:39:35.712Z",
+              "__v": 0
+            };
+            setNotes(notes.concat(note))
+
+          }
+
+          //Delete a Note
+          const deleteNote = () =>{
+            
+          }
+
+          //Edit a Note
+          const editNote = () =>{
+
+          }
     
     return(
-        <NoteContext.Provider value={{notes, setNotes}}>
+        <NoteContext.Provider value={{notes, addNote,deleteNote,editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
