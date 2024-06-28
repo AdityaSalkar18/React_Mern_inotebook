46d44cc2c962dc882950bf33915773efc6514806
Aditya <aditysalkar1806@gmail.com>
2024-06-28 21:16:48 +0530

    28.06.2024(v69)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -72,6 +72,7 @@ router.post('/login',
       
    ] 
 ,async (req,res)=>{
+   let success = false;
    //If there are errors, return Bad request and the errors
    const errors = validationResult(req);
    if(!errors.isEmpty()){
@@ -82,12 +83,14 @@ router.post('/login',
    try {
       let user = await User.findOne({email});
       if(!user){
+         success = false
          return res.status(400).json({error: "Please try to login with correct credentials"});
       }
 
       const passwordCompare = await bcrypt.compare(password, user.password);
       if(!passwordCompare){
-         return res.status(400).json({error: "Please try to login with correct credentials"});
+         success = false
+         return res.status(400).json({success, error: "Please try to login with correct credentials"});
       }
 
      
@@ -97,7 +100,8 @@ router.post('/login',
          }
       }
       const authtoken = jwt.sign(data, JWT_SECRET);
-      res.json({authtoken})
+      success = true;
+      res.json({success,authtoken})
 
 
    } catch(error){
